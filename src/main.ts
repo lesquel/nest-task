75b798f7cf72e2efc9380812402513ff60993af7
lesquel <lesquel1319@gmail.com>
2025-05-28 22:56:07 -0500

    a

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(process.env.PORT || 8080);
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
